363141f694d596d95bc4f009b18f03734a16cf8c
jbales20 <69781414+jbales20@users.noreply.github.com>
2020-09-22 20:52:51 -0400

    Added information to README.md

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Chat Bot Assignment
+The bot, Alexandria, is here to chat with you about what you have been reading recently.
+The bot was coded using Python 3.7. Will ask one or two questions per cycle until the user says 'done'
--- program.py
+++ program.py
@@ -2,13 +2,14 @@ import random
 
 response1 = ["Alexandria: That's awesome, what book are you reading?", "Alexandria: That's cool. what is your book that you're currently reading, or last read about?", "Alexandria: What do you enjoy most about the book you're currently reading, or last read?"]
 response2 = ["Alexandria: You should, reading is fun.", "Alexandria: That's too bad, you should check out my library. Wait, nevermind, there was a fire", "Alexandria: I enjoy reading myself."]
-# I used randint instead of choice so I can use detect the index to ask more than one question depending on the previous response.
 
 
 print("Hey there, I'm a Alexandria. I am kind of a book nerd. So I'll just ask book related questions.") 
 print("For the first question it's yes or no, the rest answer however you'd like. Type 'done' when you're ready to leave.")
 
 print("")
+
+# I used randint instead of choice so I can detect the index to ask more than one question depending on the previous response.
 
 def get_bot_response(user_response):
     which = random.randint(0, (len(response1)-1))
